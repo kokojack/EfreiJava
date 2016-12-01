9610d0c9eb6ecfe0efea3c99f6b43cf302613efd
koko <koko@10.10.23.64>
2016-12-01 11:23:34 +0100

    Implémentation d'une file

---
+++ Efrei/src/sdFile/File.java
@@ -0,0 +1,27 @@
+package sdFile;
+
+public class File {
+	
+	private int contenu [];
+	private int debut;
+	private int fin;
+	private int cpt;
+	
+	public File ()
+	{
+		debut = 0;
+		fin = 0;
+		cpt = 0;
+		contenu = null;
+	}
+
+	public void push ()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
